d010f80501ca4de5dae0fbb5c8d3406e28f5c420
Costa Paraskevopoulos <costa.paraskevopoulos@isentia.com>
2023-05-03 23:09:50 +1000

    update tests

--- src/app/tax-input/tax-input.component.spec.ts
+++ src/app/tax-input/tax-input.component.spec.ts
@@ -1,7 +1,20 @@
+import { TestBed } from '@angular/core/testing';
 import { TaxInputComponent } from './tax-input.component';
 
 describe('TaxInputComponent', () => {
 
+  it('should show image', () => {
+    const fixture = TestBed.createComponent(TaxInputComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('img').src).toContain('https://cdn.shopify.com');
+  });
 
+  it('should show heading', () => {
+    const fixture = TestBed.createComponent(TaxInputComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Calculate your income tax');
+  });
 
 });
