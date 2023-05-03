9408a17db4e18666626e6fb91519519921a478cc
Costa Paraskevopoulos <costa.paraskevopoulos@isentia.com>
2023-05-04 00:40:32 +1000

    Add an animation transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, animate, style, query, transition } from '@angular/animations';
 
 import { TaxInfo } from './components/tax-form/tax-form.component';
+
+// https://stackoverflow.com/questions/47248898/angular-4-5-6-7-simple-example-of-slide-in-out-animation-on-ngif
+const slideInAnimation = [
+  query(':enter', [style({ transform: 'translateX(100%)' }), animate('1s ease-out', style({ transform: 'translateX(0%)' }))], {
+    optional: true,
+  }),
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', slideInAnimation),
+    ]),
+  ]
 })
 export class AppComponent implements OnInit {
 
